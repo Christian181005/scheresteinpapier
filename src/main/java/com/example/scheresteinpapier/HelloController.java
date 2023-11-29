6852cc736e12e61dc480400a000077c2376a8a85
Manuel <pichlermanuel1273@gmail.com>
2023-11-29 11:13:06 +0100

    computerauswahl kann generiert werden

--- src/main/java/com/example/scheresteinpapier/HelloController.java
+++ src/main/java/com/example/scheresteinpapier/HelloController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
+import java.util.Random;
+
 public class HelloController {
     @FXML
     public ImageView schere;
@@ -17,17 +19,34 @@ public class HelloController {
     private String eigeneAuswahl;
     private String computerAuswahl;
     private int comuterAuswahlGenerator;
+    Random random = new Random();
 
     @FXML
     protected void onSchereClicked() {
         eigeneAuswahl = "schere";
+        setComuterAuswahlGenerator();
     }
     @FXML
     protected void onSteinClicked() {
         eigeneAuswahl = "stein";
+        setComuterAuswahlGenerator();
     }
     @FXML
     protected void onPapierClicked() {
         eigeneAuswahl = "papier";
+        setComuterAuswahlGenerator();
+    }
+    protected void setComuterAuswahlGenerator(){
+        comuterAuswahlGenerator=random.nextInt(3) + 1;
+        if (comuterAuswahlGenerator==1){
+            computerAuswahl="schere";
+        } else if (comuterAuswahlGenerator==2){
+            computerAuswahl="Stein";
+        } else{
+            computerAuswahl="Papier";
+        }
+    }
+    protected void selectWinner(){
+
     }
 }
